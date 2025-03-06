77bf854b46e6d76f2de30c018237385d17587f81
Toshihito Kikuchi <k.toshihito@yahoo.de>
2025-03-06 12:22:15 -0800

    Optional parameters

---
+++ 02-compute/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+CMD=/app/bin/t
+
+if [ -n "${DAEMON_MODE}" ]; then
+  CMD="$CMD -d"
+fi
+
+if [ -n "${MATRIX_SIZE}" ]; then
+  CMD="$CMD
+    -wA=${MATRIX_SIZE}
+    -hA=${MATRIX_SIZE}
+    -wB=${MATRIX_SIZE}
+    -hB=${MATRIX_SIZE}
+  "
+fi
+
+exec ${CMD}
