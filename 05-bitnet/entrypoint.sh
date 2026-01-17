3b376c5f587822d8a8c0f1eeb147882c04c04eb5
Toshihito Kikuchi <k.toshihito@yahoo.de>
2026-01-17 00:01:19 -0800

    Split build and run for Bitnet

--- 05-bitnet/entrypoint.sh
+++ 05-bitnet/entrypoint.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e
-
-if [ ! -f "models/BitNet-b1.58-2B-4T/ggml-model-i2_s.gguf" ]; then
-  hf download microsoft/BitNet-b1.58-2B-4T-gguf \
-    --local-dir models/BitNet-b1.58-2B-4T
-fi
-if [ ! -f "build/bin/llama-server" ]; then
-  setup_cmd="python setup_env.py -md models/BitNet-b1.58-2B-4T -q i2_s"
-  echo ${setup_cmd}
-  ${setup_cmd}
-fi
-
 open-webui serve &
-
-build/bin/llama-server \
-  --model models/BitNet-b1.58-2B-4T/ggml-model-i2_s.gguf \
+./llama-server \
+  --model models/ggml-model-i2_s.gguf \
+  --host 0.0.0.0 \
   --port ${BACKEND_PORT} \
   --ctx-size 2048
